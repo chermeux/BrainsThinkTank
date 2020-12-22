e4d412f2fb9dace9e5244f3ccd7a89a50364c3a8
chermeux <clement.chermeux@imt-bs.eu>
2020-12-23 00:38:56 +0100

    création de tout le haut de page

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" href="style.css"/>
+    <link rel="stylesheet" media="screen and (max-width: 1280px)" href="style_petitecran.css"/>
+    <link rel="shortcut icon" href="images/1_generales/logo_brains.ico" /> // to do mettre l'image dans le dossier et créer le dossier
+    <title>Brains Think Tank</title>
+</head>
+
+<body>
+        <header>
+            <div id="logo">
+                <a href="index.php"><img src="images/1_generales/logo_brains.png" alt="Brains Think Tank" /></a>
+            </div>
+            <div id="barreNav">
+                <nav>
+                    <ul>
+                        <li id="Accueil"><a href="index.php">Accueil</a></li>
+                        <li id="evenement"><a href="Evenements.php">Evenements</a></li>
+                        <li id="articles"><a href="articles.php">Articles</a></li>
+                        <li id="Club"><a href="Club.php">Club</a></li>
+                        <li id="contact"><a href="contact.php">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
