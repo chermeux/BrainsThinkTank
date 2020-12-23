b4df5d2e1f7a95947e0100f911ae7f0778fd73d5
chermeux <clement.chermeux@imt-bs.eu>
2020-12-23 11:34:33 +0100

    modifications de certains éléments dans style.css

--- header.php
+++ header.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8"/>
-    <link rel="stylesheet" href="style.css"/>
-    <link rel="stylesheet" media="screen and (max-width: 1280px)" href="style_petitecran.css"/>
-    <link rel="shortcut icon" href="images/1_generales/logo_brains.ico" /> /* TODO mettre l'image dans le dossier et créer le dossier */
-    <title>Brains Think Tank</title>
-</head>
-
-<body>
         <header>
             <div id="logo">
                 <a href="index.php"><img src="images/1_generales/logo_brains.png" alt="Brains Think Tank" /></a>
@@ -19,7 +8,7 @@
                         <li id="accueil"><a href="index.php">Accueil</a></li>
                         <li id="evenement"><a href="Evenements.php">Evenements</a></li>
                         <li id="articles"><a href="articles.php">Articles</a></li>
-                        <li id="club"><a href="Club.php">Club</a></li>
+                        <li id="club"><a href="club.php">Club</a></li>
                         <li id="contact"><a href="contact.php">Contact</a></li>
                     </ul>
                 </nav>
--- index.php
+++ index.php
@@ -1,6 +1,24 @@
-<?php include("header.php"); ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" href="style.css"/>
+        <link rel="stylesheet" media="screen and (max-width: 1280px)" href="style_petitecran.css"/>
+        <link rel="shortcut icon" href="images/1_generales/logo_brains.ico" /> /* TODO mettre l'image dans le dossier et créer le dossier */
+        <title>Brains Think Tank</title>
+    </head>
 
+    <body>
+        <div>
+             <?php include("header.php"); ?>
+         </div>
 
+         <div>
 
+         </div>
 
-<?php include("footer.php"); ?>
+         <div>
+             <?php include("footer.php"); ?>
+         </div>
+    </body>
+</html>
