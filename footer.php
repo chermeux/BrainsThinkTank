0602058f2c7304d507c7bc5db0c9d0e5237aa7c4
chermeux <clement.chermeux@imt-bs.eu>
2020-12-23 11:37:10 +0100

    modifications footer

--- footer.php
+++ footer.php
@@ -1,21 +1,18 @@
-    <div>
-        <footer id="footer">
+<div>
+    <footer id="footer">
+        <div id="haut_du_footer">
+            <ul>
+                <li id="MentionsLegales"><a href="#">Mentions Légales</a></li>
+                <li id="Facebook"><a href="#"><img src="#" alt="logoFacebook"></a></li>
+                <li id="Instagram"><a href="#"><img src="#" alt="logoInstagram"></a></li>
+                <li id="Linkedin"><a href="#"><img src="#" alt="logoLinkedin"></a></li>
+            </ul>
+        </div>
 
-            <div id="haut_du_footer">
-                <ul>
-                    <li id="MentionsLegales"><a href="#">Mentions Légales</a></li>
-                    <li id="Facebook"><a href="#"><img src="#" alt="logoFacebook"></a></li>
-                    <li id="Instagram"><a href="#"><img src="#" alt="logoInstagram"></a></li>
-                    <li id="Linkedin"><a href="#"><img src="#" alt="logoLinkedin"></a></li>
-                </ul>
-            </div>
+        <div id="bas_footer">
+            <li id="Numero"><p>Numéro de téléphone</p></li>
+            <li id="mail"><p>BrainsThinTank@gmail.com</p></li>
+        </div>
 
-            <div id="bas_footer">
-                <li id="Numero"><p>Numéro de téléphone</p></li>
-                <li id="mail"><p>BrainsThinTank@gmail.com</p></li>
-            </div>
-
-        </footer>
-    </div>
-    </body>
-</html>
+    </footer>
+</div>
