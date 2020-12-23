581d89b9c16c015e428da8d6c657d14c8d03929b
Julien LAURENCEAU <julien.laurenceau@etu.univ-nantes.fr>
2020-12-23 14:38:30 +0100

    creation de bdd

---
+++ sql/bdd.md
@@ -0,0 +1,27 @@
+#Base de donnes
+
+# USER
+    * ID
+    * NOM
+    * MOTSDEPASSE
+    * MAIL
+# LOG
+# EVENEMENT
+    * ID
+    * IMAGE
+    * TITRE
+    * TEXTE
+    * DATE
+    * LIEU
+# ARTICLES
+    * ID
+    * IMAGE
+    * TITRE
+    * TEXTE
+    * DATEDERMODIF
+# CLUBS
+    * ID
+    * IMAGE
+    * NOM
+    * TEXTE
+    * DATEDERMODIF
---
+++ sql/gen_bdd.sql
@@ -0,0 +1,47 @@
+drop DATABASE if exists brains;
+create database brains;
+use brains;
+create table articles
+(
+    id           int                                not null
+        primary key,
+    image        varchar(255)                       null,
+    titre        varchar(255)                       not null,
+    texte        longtext                           null,
+    datedermodif datetime default CURRENT_TIMESTAMP not null
+);
+
+create table clubs
+(
+    id           int                                not null
+        primary key,
+    image        varchar(255)                       null,
+    nom          varchar(255)                       not null,
+    texte        longtext                           null,
+    datedermodif datetime default CURRENT_TIMESTAMP null
+);
+
+create table evenements
+(
+    id    int          not null
+        primary key,
+    image varchar(255) null,
+    Titre varchar(255) not null,
+    texte longtext     null,
+    date  date         null,
+    lieu  varchar(512) null
+);
+
+create table user
+(
+    ID       int          not null
+        primary key,
+    NAME     varchar(32)  not null,
+    PASSWORD varchar(255) not null,
+    EMAIL    varchar(255) not null,
+    constraint USER_EMAIL_uindex
+        unique (EMAIL),
+    constraint USER_NAME_uindex
+        unique (NAME)
+);
+
