150fc3b688b787d0e8dee17862c7e76dd1f67e92
chermeux <clement.chermeux@imt-bs.eu>
2020-12-28 16:19:29 +0100

    modifications et créatio fichier articles.php

--- pages/articles.php
+++ pages/articles.php
@@ -1,3 +1,37 @@
+<?php
+try
+{
+    $bdd = new PDO("mysql:host=localhost;dbname=", "root", "");
+    $bdd ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(Exception $e)
+{
+    die("Une erreur a été trouvé : " . $e->getMessage());
+}
+$bdd->query("SET NAMES UTF8");
+
+if (isset($_GET["s"]) AND $_GET["s"] == "Rechercher")
+{
+    $_GET["terme"] = htmlspecialchars($_GET["terme"]); //pour sécuriser le formulaire contre les intrusions html
+    $terme = $_GET["terme"];
+    $terme = trim($terme); //pour supprimer les espaces dans la requête de l'internaute
+    $terme = strip_tags($terme); //pour supprimer les balises html dans la requête
+
+    if (isset($terme))
+    {
+        $terme = strtolower($terme);
+        $select_terme = $bdd->prepare("SELECT TITRE, TEXTE FROM ARTICLES WHERE TITRE LIKE ? OR TEXTE LIKE ?");
+        $select_terme->execute(array("%".$terme."%", "%".$terme."%"));
+    }
+    else
+    {
+        $message = "Vous devez entrer votre requete dans la barre de recherche";
+    }
+}
+?>
+
+
+
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/html">
     <head>
@@ -15,16 +49,27 @@
 
     <div>
         <div id="barre_de_recherche">
+            <form action ="" method = "get" enctype="multipart/form-data">
+                <input type = "search" name = "terme">
+                <input type = "submit" name = "s" value = "Rechercher">
+            </form>
+            <?php
+            while($terme_trouve = $select_terme->fetch())
+            {?>
 
-           /* CREER BARRE DE RECHERCHE */
-
-
-
-
+            <a href="<?= $terme_trouve['LIEN'] ?>" target="_blank"> // TODO Rajouter lien dans bdd
+            <div id="rectangle_derniersarticles">
+                <img class="images_article" src="images/articles/<?= $terme_trouve['IMAGE'] ?>"/>
+                <h3><?= $terme_trouve['TITRE'] ?></h3>
+                <p class="dateDeModif"> <?= $terme_trouve['DATEDEMODIF'] ?></p>
+            </div>
+            </a>
+            <?=}
+            $select_terme->closeCursor();
+            ?>
         </div>
         <h2>Tous nos articles ! </h2>
         <article>
-            <h2>Nos derniers articles</h2>
             <?php
             $datestocke = "00/00/0000";
             $idstocke = "0";
@@ -46,26 +91,13 @@
             $articles = $bdd->query("SELECT * FROM ARTICLES WHERE ID='{$idstockeprecedent}'");
             $artiun = $articles->fetch();
             ?>
-            <a href="<?= $artiun['lien'] ?>" target="_blank"> // TODO Rajouter lien dans bdd
+            <a href="<?= $artiun['LIEN'] ?>" target="_blank"> // TODO Rajouter lien dans bdd
                 <div id="rectangle_derniersarticles">
                     <img class="images_article" src="images/articles/<?= $artiun['IMAGE'] ?>"/>
                     <h3><?= $artiun['TITRE'] ?></h3>
                     <p class="dateDeModif"> <?= $artiun['DATEDEMODIF'] ?></p>
                 </div>
             </a>
-
-            <?php
-            $articles = $bdd->query("SELECT * FROM ARTICLES WHERE ID='{$idstocke}'");
-            $artiDeux = $articles->fetch();
-            ?>
-            <a href="<?= $artiDeux['lien'] ?>" target="_blank"> // TODO Rajouter lien dans bdd
-                <div id="rectangle_derniersarticles">
-                    <img class="images_article" src="images/articles/<?= $artiDeux['IMAGE'] ?>"/>
-                    <h3><?= $artiDeux['TITRE'] ?></h3>
-                    <p class="dateDeModif"> <?= $artiDeux['DATEDEMODIF'] ?></p>
-                </div>
-            </a>
-
 
         </article>
     </div>
