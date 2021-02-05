d7fa3a04f73bb4cafe8e55041ffd4a03a6bcfc98
chermeux <clement.chermeux@imt-bs.eu>
2021-02-05 23:45:31 +0100

    modification style.css

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
         <h2>Qui sommes-nous ?</h2>
         <p>Raptim igitur properantes ut motus sui rumores celeritate nimia praevenirent, vigore corporum ac levitate confisi per flexuosas semitas ad summitates collium tardius evadebant. et cum superatis difficultatibus arduis ad supercilia venissent fluvii Melanis alti et verticosi, qui pro muro tuetur accolas circumfusus, augente nocte adulta terrorem quievere paulisper lucem opperientes. arbitrabantur enim nullo inpediente transgressi inopino adcursu adposita quaeque vastare, sed in cassum labores pertulere gravissimos. Ex his quidam aeternitati se commendari posse per statuas aestimantes eas ardenter adfectant quasi plus praemii de figmentis aereis sensu carentibus adepturi, quam ex conscientia honeste recteque factorum, easque auro curant inbracteari, quod Acilio Glabrioni delatum est primo, cum consiliis armisque regem superasset Antiochum. quam autem sit pulchrum exigua haec spernentem et minima ad ascensus verae gloriae tendere longos et arduos, ut memorat vates Ascraeus, Censorius Cato monstravit. qui interrogatus quam ob rem inter multos... statuam non haberet malo inquit ambigere bonos quam ob rem id non meruerim, quam quod est gravius cur inpetraverim mussitare.</p>
     </article>
-    <article>
+    <article id="DerniersArticles">
         <h2>Nos derniers articles</h2>
         <?php
         $bdd = new PDO('mysql:host=localhost;dbname=brains;charset=utf8', 'root', 'root');
@@ -33,7 +33,7 @@
             </a>
         <?php } ?>
     </article>
-    <article>
+    <article id="ProcahinsEvenements">
         <h2>Nos prochains événements</h2>
         <?php
         $bdd = new PDO('mysql:host=localhost;dbname=brains;charset=utf8', 'root', 'root');
