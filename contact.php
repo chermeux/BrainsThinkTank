d7580ed7e176638dd3fcb1ce4f72061d6e3f1306
chermeux <clement.chermeux@imt-bs.eu>
2021-02-20 17:13:12 +0100

    css

---
+++ contact.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" href="style.css"/>
+    <link rel="stylesheet" media="screen and (max-width: 1280px)" href="style_petitecran.css"/>
+    <link rel="shortcut icon" href="images/1_generales/logo_brains.ico" /> <!-- TODO mettre l'image dans le dossier et créer le dossier -->
+    <title>Agora</title>
+</head>
+
+<body>
+<div>
+    <?php include("header.php"); ?>
+</div>
+
+<div>
+    <article>
+        <p><img class="image_agora_contact" src="images/contact/image_agora_contact.jpg" alt="image debat agora"/></p>
+    </article>
+    <article id="ContactUs">
+        <h2>Nous contacter !</h2>
+        <p>Adresse : 9 rue Charles Fourier, 91000 Evry-Courcouronnes</p>
+        <p>Mail : Agora@gmail.com</p>
+    </article>
+</div>
+
+<div>
+    <?php include("footer.php"); ?>
+</div>
+</body>
+</html>
--- footer.php
+++ footer.php
@@ -2,15 +2,15 @@
        <div id="haut_du_footer">
            <ul>
               <li id="MentionsLegales"><a href="#">Mentions Légales</a></li>
-              <li id="Facebook"><a href="#"><img src="#" alt="logoFacebook"></a></li>
-              <li id="Instagram"><a href="#"><img src="#" alt="logoInstagram"></a></li>
-              <li id="Linkedin"><a href="#"><img src="#" alt="logoLinkedin"></a></li>
+              <li id="Facebook"><a href="#"><img src="images/1_generales/logoFacebook.png" alt="logoFacebook"></a></li>
+              <li id="Instagram"><a href="#"><img src="images/1_generales/logoinstagram.png" alt="logoInstagram"></a></li>
+              <li id="Linkedin"><a href="#"><img src="images/1_generales/logolinkedin.png" alt="logoLinkedin"></a></li>
            </ul>
        </div>
 
        <div id="bas_footer">
            <ul>
-              <li id="Numero"><p>Numéro de téléphone</p></li>
+              <li id="Numero"><p>9 rue Charles Fourirer, 91000 Evry-Courcouronnes</p></li>
               <li id="mail"><p>Agora@gmail.com</p></li>
            </ul>
        </div>
